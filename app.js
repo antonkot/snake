76992207d85caadc45ff37334833147a1d23a399
Anton Kot <mail@anton-kot.ru>
2018-11-20 08:55:07 +0300

    some code refactoring

--- app.js
+++ app.js
@@ -11,21 +11,23 @@ let grid = []
 
 // Returns empty grid
 function getEmptyGrid() {
-	let temp = []
-	for (var x = 0; x < CELL_COUNT; x++) {
-		temp.push([])
-		for (var y = 0; y < CELL_COUNT; y++) {
-			if (
-				x == 0 || y == 0 ||
-				x == CELL_COUNT - 1 || y == CELL_COUNT - 1
-			) {
-				temp[x].push(-1)
-			} else {
-				temp[x].push(0)
-			}
-		}
-	}
-	return temp
+    let result = []
+    for (var x = 0; x < CELL_COUNT; x++) {
+        result.push([])
+        for (var y = 0; y < CELL_COUNT; y++) {
+            if (
+                x == 0 ||
+                y == 0 ||
+                x == CELL_COUNT - 1 ||
+                y == CELL_COUNT - 1
+            ) {
+                result[x].push(-1)
+            } else {
+                result[x].push(0)
+            }
+        }
+    }
+    return result
 }
 
 grid = getEmptyGrid()
